dca5e0cbb55ee783d7966a89fd8653b2e6f22951
YukC92 <yukfaichan92@gmail.com>
2022-08-17 12:43:07 -0700

    get and put user backend done

--- e-commerce-backend-main/src/main/java/com/revature/repositories/UserRepository.java
+++ e-commerce-backend-main/src/main/java/com/revature/repositories/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByEmailAndPassword(String email, String password);
+    
+    Optional<User> findById(int id);
 }
