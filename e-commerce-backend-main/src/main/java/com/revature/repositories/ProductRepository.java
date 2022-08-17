52dc9efe099b19984ab7045b9034f2e1d2a5cb39
Vahe Aslanyan <vaheaslanyan@macbook1hunnid.local>
2022-08-17 14:26:22 -0700

    Add methods in ProductRepository

--- e-commerce-backend-main/src/main/java/com/revature/repositories/ProductRepository.java
+++ e-commerce-backend-main/src/main/java/com/revature/repositories/ProductRepository.java
@@ -1,7 +1,28 @@
 package com.revature.repositories;
 
 import com.revature.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+	
+	// For searching by partial match (similar to SQL LIKE command) 
+	public List<Product> findByDescriptionContaining(String description);
+	
+//	public List<Product> findByNameContaining(String name);
+	
+	public List<Product> findByIdContaining(String id);
+	
+	// Searching for a match in either column
+	@Query(value = "SELECT * FROM product WHERE description = :searchWord OR name = :searchWord OR id = :searchWord;", nativeQuery = true)
+	public List<Product> findByAny(String searchWord);
+	
+//	@Query(value = "UPDATE productimage SET product_image = :productImage WHERE product_id = :id", nativeQuery = true)
+	
+	
 }
