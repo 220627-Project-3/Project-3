887fde593224aaf2ff18ab9f1f2c7864ae261155
Dino Goudas <Constantine930@revature.net>
2022-08-20 17:45:50 -0400

    Got two tests in

--- e-commerce-frontend-angular-main/src/app/services/product.service.spec.ts
+++ e-commerce-frontend-angular-main/src/app/services/product.service.spec.ts
@@ -3,20 +3,49 @@ import { NavbarComponent } from '../components/navbar/navbar.component';
 import { HttpClient } from '@angular/common/http';
 import { ProductService } from './product.service';
 import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
-
+import { createSpyFromClass, Spy } from 'jasmine-auto-spies';
+import { Product } from '../models/product';
 
 
 
 describe('ProductService', () => {
   let service: ProductService;
+  let httpSpy: Spy<HttpClient>;
+  let fakeProduct: Product =
+    {
+      id: 2,
+      name: "Fake Product",
+      quantity: 100,
+      price: 10,
+      description:"I am Fake",
+      image:"fake"
+    };
+  let fakeProducts: Product[]=[{
+    id: 1,
+    name: "Fake Product",
+    quantity: 100,
+    price: 10,
+    description:"I am Fake",
+    image:"fake"
+  },{
+    id: 2,
+    name: "Faker Product",
+    quantity: 189,
+    price: 3.50,
+    description:"I am Faker",
+    image:"fake"
+  }];
+    
   
   beforeEach(() => {
-    TestBed.configureTestingModule({imports: [ HttpClientTestingModule ]});
-    
-    service = TestBed.inject(ProductService);
-    
+    TestBed.configureTestingModule({imports: [ HttpClientTestingModule ],providers: [
+      ProductService,
+      { provide: HttpClient, useValue: createSpyFromClass(HttpClient) }
+    ]
   });
-
+      service = TestBed.inject(ProductService);
+      httpSpy = TestBed.inject<any>(HttpClient);
+});
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
@@ -33,16 +62,31 @@ describe('ProductService', () => {
     }
     expect(service.idPr==1).toBeTruthy();
   });
-  it('get Product By Id',()=>{
-      let z =service.getProductById(1);
-    
-    expect(z).toBeTruthy();
+
+  it('get Products By Id',(done: DoneFn)=>{
+    httpSpy.get.and.nextWith(fakeProduct);
+    service.getProductById(2).subscribe(
+      Product => {
+        expect(Product.id).toEqual(fakeProduct.id);
+        done();
+      },
+      done.fail
+    );
+    expect(httpSpy.get.calls.count()).toBe(1);
   });
 
-  it('get Products By Name',()=>{
-    let z =service.getProductsByName("sneak");
+  it('get Products By Name',(done: DoneFn)=>{
+    httpSpy.get.and.nextWith(fakeProducts);
+    service.getProductsByName("Fake Product").subscribe(
+      Products => {
+        expect(Products).toHaveSize(fakeProducts.length);
+        expect(Products[1].name).toEqual(fakeProducts[1].name);
+        done();
+      },
+      done.fail
+    );
+    expect(httpSpy.get.calls.count()).toBe(1);
+  });
   
-  expect(z).toBeTruthy();
 });
   
-});
