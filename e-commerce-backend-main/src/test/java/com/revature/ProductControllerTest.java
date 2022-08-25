e1f60048945beb780841618c3867382dc93e829f
unknown <fred.kelemen99@gmail.com>
2022-08-25 12:22:34 -0400

    Stupid thing makes me commit

--- e-commerce-backend-main/src/test/java/com/revature/ProductControllerTest.java
+++ e-commerce-backend-main/src/test/java/com/revature/ProductControllerTest.java
@@ -2,6 +2,7 @@ package com.revature;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
