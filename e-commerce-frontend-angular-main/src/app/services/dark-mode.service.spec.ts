81a045674b45ceb8b12eedff094ef52250487100
DamDanyo <dmitch1423@gmail.com>
2022-08-28 23:18:40 -0400

    more dark mode testing

--- e-commerce-frontend-angular-main/src/app/services/dark-mode.service.spec.ts
+++ e-commerce-frontend-angular-main/src/app/services/dark-mode.service.spec.ts
@@ -3,6 +3,7 @@ import { DarkModeService } from './dark-mode.service';
 
 describe('DarkModeService', () => {
   let service: DarkModeService;
+  let spy: any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -16,5 +17,20 @@ describe('DarkModeService', () => {
   it('should have a method for changing the theme called toggleDarkTheme()', () => {
     //check the service for the toggle function
     expect(service.toggleDarkTheme).toMatch('toggleDarkTheme()');
+  });
+
+  it('should change the theme to "dark-theme" when called', () => {
+    //toggle dark theme on
+    service.toggleDarkTheme();
+    //check for 'dark-theme' class in body tag
+    expect(document.body.classList).toContain('dark-theme');
+  });
+  it('should change the theme to "light-theme" when called twice', () => {
+    //toggle dark theme on
+    service.toggleDarkTheme();
+    //toggle it back off
+    service.toggleDarkTheme();
+    //check for 'dark-theme' class in body tag
+    expect(document.body.classList).toMatch('');
   });
 });
