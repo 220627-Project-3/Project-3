c950b30938b940dec628f18ea25cdf8f0b54b420
DamDanyo <dmitch1423@gmail.com>
2022-08-28 21:25:54 -0400

    dark mode test 1

--- e-commerce-frontend-angular-main/src/app/services/dark-mode.service.spec.ts
+++ e-commerce-frontend-angular-main/src/app/services/dark-mode.service.spec.ts
@@ -13,4 +13,8 @@ describe('DarkModeService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('#toggleDarkTheme should change the theme', () => {
+    expect(service.toggleDarkTheme).toBeTruthy;
+  });
 });
