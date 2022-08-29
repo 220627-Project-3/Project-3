573fde9409768030a8d6f022248f4207c4e8fa56
Fred374 <60891034+Fred374@users.noreply.github.com>
2022-08-29 17:42:14 -0400

    Update karma.conf.js

--- e-commerce-frontend-angular-main/karma.conf.js
+++ e-commerce-frontend-angular-main/karma.conf.js
@@ -13,6 +13,9 @@ module.exports = function (config) {
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    FirefoxHeadless:{
+      base: 'Firefox', flags: ['--headless']
+    }
     client: {
       jasmine: {
         // you can add configuration options for Jasmine here
