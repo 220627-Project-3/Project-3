6dcca905590677bdb7081553f446c3eb86732db6
DamDanyo <dmitch1423@gmail.com>
2022-08-29 16:34:49 -0400

    removed unnecessary schemas from new tests

--- e-commerce-frontend-angular-main/src/app/components/footer/footer.component.spec.ts
+++ e-commerce-frontend-angular-main/src/app/components/footer/footer.component.spec.ts
@@ -1,4 +1,4 @@
-import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
@@ -10,7 +10,7 @@ describe('FooterComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FooterComponent],
-      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FooterComponent);
